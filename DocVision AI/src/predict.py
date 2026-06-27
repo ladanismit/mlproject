f912122136aa9c9a7469ee8f671fc3db4033acba
LADANI SMITKUMAR BHAVINBHAI <82303296+ladanismit@users.noreply.github.com>
2026-06-27 21:36:51 +0530

    Add files via upload

--- DocVision AI/src/predict.py
+++ DocVision AI/src/predict.py
@@ -179,7 +179,7 @@ def main():
     parser.add_argument(
         "--image",
         type=str,
-        default=r"D:\MLPs\DocVision AI\data\raw\Invoice\X51005442344.jpg",
+        default=r"D:\MLPs\DocVision AI\data\raw\test\X51005433518.jpg",
         help="Path to the document image file (JPG, PNG, BMP, TIFF, etc.)",
     )
     parser.add_argument(
